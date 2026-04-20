2e762490da5d8943733c6483783cad642b21172e
Victor Marcos <victoreugenio_mheras@alumnos-santagema.es>
2026-04-20 10:44:35 +0200

    cambio desde pc clase

--- BASE DE DATOS/Programacion MariaDB/Ejercicio Funciones.sql
+++ BASE DE DATOS/Programacion MariaDB/Ejercicio Funciones.sql
@@ -1,18 +1,22 @@
+--CORREGIDO (SIN CORREGIR EN CLASE)
 DELIMITER //
+-- Es una funcion porque pone "Se espera tener este resultado en un informe"
+CREATE function becas_progresivas (
+    p_id_alumno INT,
+    p_numero_curso INT )
+    
+    RETURNS decimal(10,2)
 
-CREATE PROCEDURE becas_progresivas (
-    IN p_id_alumno INT,
-    IN p_numero_curso INT,
-    OUT p_ahorro_total decimal(10,2)
-)
 
 --Salida
 
+DETERMINISTIC;
+
 principal: BEGIN
-    Declare v_numero_veces INT;
+    Declare v_numero_veces INT; --Default 1;
     Declare v_precio_curso decimal(10,2);
-    Declare v_ahorro_total decimal(10,2);
-    Declare v_descuento decimal(5,2);
+    Declare v_ahorro_total decimal(10,2); --Se puede usar aqui un "Default 0;" en vez del SET.
+    Declare v_descuento decimal(5,2); --Default 0;
          SELECT c.precio INTO v_precio_curso 
          FROM matriculas m JOIN cursos c 
          ON m.id_curso = c.id_curso 
@@ -20,15 +24,15 @@ principal: BEGIN
     
 
     SET v_numero_veces = 1;
-    SET v_ahorro_total= 0;
+    SET v_ahorro_total = 0;
 
     WHILE v_numero_veces < p_numero_cursos DO
 
         IF v_numero_veces = 1 THEN
-            SET v_descunto = 0;
+            SET v_descuento = 0;
 
         ELSE IF v_numero_veces = 2 THEN
-            SET v_descunto = 0.05;
+            SET v_descuento = 0.05;
 
         ELSE IF v_numero_veces =< 3 THEN
             SET v_descuento = 0.10;
